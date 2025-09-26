b35fcd3fb666dcf638f16ff63ef79df8cafc8d57
t1ctac19 <2497735@cmontmorency.qc.ca>
2025-09-26 11:29:38 -0400

    avancement dans le jeu

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var vitesse_marche = 500.0
+@export var vitesse_course = 750.0
+@export_range(0, 1) var acceleration = 0.2
+@export_range(0, 1) var deceleration = 0.2
+
+@export var force_saut = -600
+@export_range(0, 1) var deceleration_saut_relacher = 0.5
+
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +17,24 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("saut") and (is_on_floor() or is_on_wall()):
+		velocity.y = force_saut
+	
+	if Input.is_action_just_released("saut") and velocity.y < 0:
+		velocity.y *= deceleration_saut_relacher
+	
+	var vitesse
+	if Input.is_action_pressed("course"):
+		vitesse = vitesse_course
+	else:
+		vitesse = vitesse_marche
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("gauche", "droite")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x ,direction * vitesse, vitesse * acceleration )
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, vitesse_marche * deceleration)
 
 	move_and_slide()
